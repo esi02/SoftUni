5b69beb0309cc4547059bb02fe2acfa22afbd6e5
Esin Mustafa <esinmustafa79@gmail.com>
2022-03-09 15:53:56 +0200

    Adding files

---
+++ C# OOP/CarRacing/Models/Cars/Car.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CarRacing.Models.Cars.Contracts;
+using CarRacing.Utilities.Messages;
+
+namespace CarRacing.Models.Cars
+{
+    public abstract class Car : ICar
+    {
+        private string _make;
+        private string _model;
+        private string _vin;
+        private int _horsePower;
+        private double _fuelAvailable;
+        private double _fuelConsumptionPerRace;
+
+        public Car(string make, string model, string VIN, int horsePower, double fuelAvailable, double fuelConsumptionPerRace)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.VIN = VIN;
+            this.HorsePower = horsePower;
+            this.FuelAvailable = fuelAvailable;
+            this.FuelConsumptionPerRace = fuelConsumptionPerRace;
+        }
+        public string Make
+        {
+            get => this._make;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidCarMake);
+                }
+
+                this._make = value;
+            }
+        }
+
+        public string Model
+        {
+            get => this._model;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidCarModel);
+                }
+
+                this._model = value;
+            }
+        }
+
+        public string VIN
+        {
+            get => this._vin;
+            private set
+            {
+                if (value.Length != 17)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidCarVIN);
+                }
+
+                this._vin = value;
+            }
+        }
+
+        public int HorsePower
+        {
+            get => this._horsePower;
+            protected set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidCarHorsePower);
+                }
+
+                this._horsePower = value;
+            }
+        }
+
+        public double FuelAvailable
+        {
+            get => _fuelAvailable;
+            private set
+            {
+                _fuelAvailable = value;
+                if (_fuelAvailable < 0)
+                {
+                    _fuelAvailable = 0;
+                }
+
+            }
+        }
+
+        public double FuelConsumptionPerRace
+        {
+            get => _fuelConsumptionPerRace;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidCarFuelConsumption);
+                }
+
+                _fuelConsumptionPerRace = value;
+            }
+        }
+        public virtual void Drive()
+        {
+            FuelAvailable -= FuelConsumptionPerRace;
+        }
+    }
+}
---
+++ C# OOP/CarRacing/Models/Racers/Racer.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CarRacing.Models.Cars.Contracts;
+using CarRacing.Models.Racers.Contracts;
+using CarRacing.Utilities.Messages;
+
+namespace CarRacing.Models.Racers
+{
+    public abstract class Racer : IRacer
+    {
+        private string _username;
+        private string _racingBehavior;
+        private int _drivingExperience;
+        private ICar _car;
+
+        public Racer(string username, string racingBehavior, int drivingExperience, ICar car)
+        {
+            this.Username = username;
+            this.RacingBehavior = racingBehavior;
+            this.DrivingExperience = drivingExperience;
+            this.Car = car;
+        }
+        public string Username
+        {
+            get => _username;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidRacerName);
+                }
+
+                _username = value;
+            }
+        }
+
+        public string RacingBehavior
+        {
+            get => _racingBehavior;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidRacerBehavior);
+                }
+
+                _racingBehavior = value;
+            }
+        }
+
+        public int DrivingExperience
+        {
+            get => _drivingExperience;
+            protected set
+            {
+                _drivingExperience = value;
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidRacerDrivingExperience);
+                }
+
+            }
+        }
+
+        public ICar Car
+        {
+            get => _car;
+            private set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidRacerCar);
+                }
+
+                _car = value;
+            }
+        }
+        public virtual void Race()
+        {
+            Car.Drive();
+        }
+
+        public bool IsAvailable()
+        {
+            return Car.FuelAvailable > Car.FuelConsumptionPerRace;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.GetType().Name}: {this.Username}");
+            sb.AppendLine($"--Driving behavior: {this.RacingBehavior}");
+            sb.AppendLine($"--Driving experience: {this.DrivingExperience}");
+            sb.AppendLine($"--Car: {Car.Make} {Car.Model} ({Car.VIN})");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
